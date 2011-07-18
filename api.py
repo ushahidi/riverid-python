f853934db491b1f7e088b0c861e29783ce2ffb31
Charl van Niekerk <charl@ushahidi.com>
2011-07-18 20:41:12 +0200

    Added API class

---
+++ api.py
@@ -0,0 +1,29 @@
+from user import User
+
+class API(object):
+    def changeemail(oldemail, newemail, password:
+        pass
+
+    def changepassword(email, oldpassword, newpassword):
+        pass
+
+    def checkpassword(email, password):
+        pass
+
+    def confirmemail(email, token):
+        pass
+
+    def currentsessions(session):
+        pass
+
+    def recoverpassword(email):
+        pass
+
+    def register(email, password):
+        pass
+
+    def signin(email, password):
+        pass
+
+    def signout(session):
+        pass
