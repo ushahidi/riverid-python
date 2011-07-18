376ec163937881db1311ad9d48c951de029e82f0
Charl van Niekerk <charl@ushahidi.com>
2011-07-18 23:37:58 +0200

    Various bug fixes.

--- utils/validator.py
+++ utils/validator.py
@@ -1,10 +1,12 @@
 import re
 
-class Validtor(object):
+class Validator(object):
+    @staticmethod
     def password(string):
-        if 8 < len(string) < 100:
-            raise Exception('Please provide a password between 8 and 100 characters in length.')
+        if not 8 <= len(string) <= 128:
+            raise Exception('Please provide a password between 8 and 128 characters in length.')
 
+    @staticmethod
     def email(string):
         if re.match(r'(?:^|\s)[-a-z0-9_.]+@(?:[-a-z0-9]+\.)+[a-z]{2,6}(?:\s|$)', string, re.IGNORECASE) == None:
             raise Exception('Please provide a valid email address.')
