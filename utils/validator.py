0383001113d3f798aac597fb50685455a33c35f8
Charl van Niekerk <charl@ushahidi.com>
2011-07-20 15:21:21 +0200

    Arranged methods according to alphabetical order.

--- utils/validator.py
+++ utils/validator.py
@@ -2,21 +2,21 @@ from re import match, IGNORECASE
 
 class Validator(object):
     @staticmethod
-    def password(string):
-        if not 8 <= len(string) <= 128:
-            raise Exception('Please provide a password between 8 and 128 characters in length.')
-
-    @staticmethod
     def email(string):
         if match(r'(?:^|\s)[-a-z0-9_.]+@(?:[-a-z0-9]+\.)+[a-z]{2,6}(?:\s|$)', string, IGNORECASE) == None:
             raise Exception('Please provide a valid email address.')
 
     @staticmethod
-    def token(string):
-        if match(r'^[a-zA-Z0-9]{16}$', string) == None:
-            raise Exception('Please provide a valid token.')
+    def password(string):
+        if not 8 <= len(string) <= 128:
+            raise Exception('Please provide a password between 8 and 128 characters in length.')
 
     @staticmethod
     def session(string):
         if match(r'^[a-zA-Z0-9]{128}$', string) == None:
             raise Exception('Please provide a valid session identifier.')
+
+    @staticmethod
+    def token(string):
+        if match(r'^[a-zA-Z0-9]{16}$', string) == None:
+            raise Exception('Please provide a valid token.')
