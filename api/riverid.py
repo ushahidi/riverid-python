1098a9d6a2f4773be6872643845e99ce3bf87e2d
Charl van Niekerk <charl@ushahidi.com>
2011-07-26 14:29:13 +0200

    Added API

--- api/riverid.py
+++ api/riverid.py
@@ -16,24 +16,35 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with RiverID.  If not, see <http://www.gnu.org/licenses/>.
 
-import flask, json
+from api import API
+from flask import Flask, make_response, request
+from inspect import getargspec
+from json import dumps
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/api/<method>')
-def api(method):
-    parameters = flask.request.args.to_dict()
-    if callback in parameters:
-        callback = parameters['callback']
-        del parameters['callback']
+@app.route('/api/<method_name>')
+def api(method_name):
+    api = API()
+    method = getattr(api, method_name)
+    method_parameters = getargspec(method).args
+    request_parameters = request.args.to_dict()
+
+    if callback in request_parameters:
+        callback = request_parameters['callback']
+        del request_parameters['callback']
     else:
         callback = False
-    result = getattr(interface, method)(**parameters)
-    response_data = json.dumps(result)
+
+    result = method(**request_parameters)
+    response_data = dumps(result)
+
     if callback:
         response_data = ''.join(callback, '(', response_data, ')')
-    flask.make_response(response_data)
+
+    response = make_response(response_data)
     response.headers['Content-Type'] = 'application/javascript; charset=UTF-8' if callback else 'application/json; charset=UTF-8'
+
     return response
 
 def main():
