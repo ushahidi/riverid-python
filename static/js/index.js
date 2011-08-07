5fd728f8bdac6855575eeb2660591958e3d365c0
Charl van Niekerk <charl@ushahidi.com>
2011-08-07 21:43:17 +0200

    Expanded login code

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,16 @@
 $('#signin button').click(function() {
+	$('#signin button, #signin input').attr('disabled', true);
 	$.getJSON('/api/signin?callback=?', {email: $('#signin-email').val(), password: $('#signin-password').val()}, function(response) {
 		if (response.success) {
-			alert('logged in');
+			localStorage.setItem('session_email', $('#signin-email').val());
+			localStorage.setItem('session_id', response.response);
+			$('#email').text($('#signin-email').val());
+			$('#signout').show();
+			$('#anonymous, #signin').hide();
+			$('#signedin').show();
 		} else {
 			$('#signin .error').text(response.error);
 		}
+		$('#signin button, #signin input').attr('disabled', false);
 	});
 });
