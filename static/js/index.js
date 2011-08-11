186fbfa90d605e027af7dcc31d3e46a29f98c643
Charl van Niekerk <charlvn@charlvn.com>
2011-08-11 17:52:44 +0300

    Added success

--- static/js/index.js
+++ static/js/index.js
@@ -21,3 +21,20 @@ $('#signin button').live('click', function() {
 		$('#signin button, #signin input').attr('disabled', false);
 	});
 });
+$('#register button').live('click', function() {
+	$('#register button, #register input').attr('disabled', true);
+	if ($('#register-email').val() == $('#register-confirm').val()) {
+		$.getJSON('/api/requestpassword?callback=?', {email: $('#register-email').val()}, function(response) {
+			if (response.success) {
+				$('#register .error').text('');
+				$('#register .success').show();
+			} else {
+				$('#register .success').hide();
+				$('#register .error').text(response.error);
+			}
+			$('#register button, #register input').attr('disabled', false);
+		});
+	} else {
+		$('#register').text('The two passwords you entered do not match. Please try again.')
+	}
+});
